f6ffc6fafde017fc76ba7acdc41f51d54a3ecd4e
React Js <reactjs11@gmail.com>
2022-08-21 20:21:46 +0300

    .

--- todolistbackend/settings.py
+++ todolistbackend/settings.py
@@ -146,7 +146,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'build/static')
 ]
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFileStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
