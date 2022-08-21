ce908a4e23cad7a1f8cd859193f4f0bb5d07c815
React Js <reactjs11@gmail.com>
2022-08-21 20:31:27 +0300

    add db

--- todolistbackend/settings.py
+++ todolistbackend/settings.py
@@ -101,8 +101,12 @@ WSGI_APPLICATION = 'todolistbackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dehhfm1bis1ckh',
+        'USER': 'rbthkngtcrhzng',
+        'PASSWORD': 'ca4a968b29cfb4810f7906093e4055b1dd9006020fe8896f70ff01ebefdb9d81',
+        'HOST': 'ec2-54-86-106-48.compute-1.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
