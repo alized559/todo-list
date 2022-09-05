3e39ec59c01d25d23f67233f33c8953aeffe5732
React Js <reactjs11@gmail.com>
2022-09-05 11:29:53 +0300

    .

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ import os
 from flask import Flask
 from flask_migrate import Migrate
 from flask_cors import CORS
+from flask.helpers import send_from_directory
 from db import db
 from todo_list import todo_list_api
 from models.todos import Todo
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='build', static_url_path='')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -14,6 +15,9 @@ db.init_app(app)
 migrate = Migrate(app, db)
 
 CORS(app, support_credentials=True)
+
+def serve():
+	return send_from_directory(app.static_folder, 'index.html')
 
 app.register_blueprint(todo_list_api)
 
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import TodoListPage from './components/todo_list_page';
+import './style.css';
+
+const App = () => {
+  return (
+    <TodoListPage />
+  );
+};
+
+export default App;
